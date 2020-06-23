98494da38b7392cf614e196daca8bc2cabc35a1a
Sarah <sarahahmad17@gmail.com>
2020-06-22 22:15:02 -0400

    Updates

---
+++ precache-manifest.072b5033630f783a5bdd797319ef359f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ecb4db95f401afd7b32ca6cf234fbe",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "8291ae7174af33975b72",
+    "url": "/github-finder/static/css/main.b4471e31.chunk.css"
+  },
+  {
+    "revision": "93e112bd94ab55f6718a",
+    "url": "/github-finder/static/js/2.3cd10ad1.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/github-finder/static/js/2.3cd10ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8291ae7174af33975b72",
+    "url": "/github-finder/static/js/main.f241443a.chunk.js"
+  },
+  {
+    "revision": "e2c8255bce1eb204bf1a",
+    "url": "/github-finder/static/js/runtime-main.bcd3cdab.js"
+  }
+]);
